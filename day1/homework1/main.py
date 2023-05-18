9e919b9ccc5aab4d137932b698edace4ebcd4282
namika2000 <114719204+namika2000@users.noreply.github.com>
2023-05-18 13:07:58 +0900

    add exception handling

--- day1/homework1/main.py
+++ day1/homework1/main.py
@@ -1,20 +1,39 @@
+import sys
+sys.path.append('../')
+from homework2.utils.read_file import read_file
 from utils.binary_search import binary_search
 from utils.dict_merge_sort import dict_merge_sort
 from utils.words_merge_sort import words_merge_sort
 
-def main(random_word: str, dictionary: list[str]) -> None:
+##CONST##
+##DICTIONARY###########
+DICTIONARY = read_file("words.txt")
+#######################
+
+def main(random_word: str) -> None:
+  if random_word == '':
+    print('文字列を入力してください')
+    return
   sorted_random_word: str = words_merge_sort(random_word)
   new_dict: list[str] = []
-  for word in dictionary:
+  for word in DICTIONARY:
     sorted_word: str = words_merge_sort(word)
     new_dict.append([sorted_word, word])
   sorted_dict: list[str] = dict_merge_sort(new_dict)
   
   ans: list[str] = binary_search(sorted_dict, sorted_random_word)
   
-  print(ans)
+  if ans is None:
+    print('anagramが見つかりません')
+    return
+  
+  print(f'{random_word}のanagramは{ans}です')
 
-dictionary = ['orange', 'apple', 'peach', 'watermelon', 'alppe', 'appel', 'a', 'app', 'apply']
-random_word = ''
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("usage: %s data_file" % sys.argv[0])
+        exit(1)
+    print('---', sys.argv[1])
+    main(sys.argv[1])
 
-main(random_word, dictionary)
+
