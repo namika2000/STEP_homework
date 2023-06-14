f2357442ab62ec04d9c918d86f368bc0ca7a76cf
namika2000 <114719204+namika2000@users.noreply.github.com>
2023-06-14 22:24:02 +0900

    add output_hoge.csv

--- week5/google-step-tsp-main/common.py
+++ week5/google-step-tsp-main/common.py
@@ -1,14 +1,15 @@
 def read_input(filename):
     with open(filename) as f:
+        print(filename)
         cities = []
         for line in f.readlines()[1:]:  # Ignore the first line.
-            xy = line.split(',')
+            xy = line.split(",")
             cities.append((float(xy[0]), float(xy[1])))
         return cities
 
 
 def format_tour(tour):
-    return 'index\n' + '\n'.join(map(str, tour))
+    return "index\n" + "\n".join(map(str, tour))
 
 
 def print_tour(tour):
